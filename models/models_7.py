6dc9be8faa5e4cb5b14212106df3ac0c4f514140
leti12 <leticia.gonzalez@quadit.mx>
2019-07-02 09:40:33 -0500

    debug

--- models/models_7.py
+++ models/models_7.py
@@ -1,20 +1,23 @@
 #-*- coding:utf-8 -*-
 from openerp.osv import fields, osv
 class academy_calificacion(osv.osv):
-   _name='academy.calificacion'
-   _description='Calificaciones de estudiantes'
-   _columns={
-        'name': fields.many2one('academy.materia', 'Materia'),
-       'calificacion': fields.float('Calificacion', digits=(3,2)),
-       'student_id':fields.many2one('academy.student','ID Referencia'),
-   }
-   _defaults={
+  _name='academy.calificacion'
+  _description='Calificaciones de estudiantes'
+  _columns={
+    'name': fields.many2one('academy.materia', 'Materia'),
+    'calificacion': fields.float('Calificacion', digits=(3,2)),
+    'student_id':fields.many2one('academy.student','ID Referencia'),
+  }
+  _defaults={
 
-       }
+  }
 class academy_materia(osv.osv):
-   _name='academy.materia'
-   _description='Materias del estudiante'
-   _columns={
-       'name':fields.char('Nombre')
-   }
-   _sql_constraints = [('name_uniq', 'unique (name)', 'El nombre de la Materia debe ser unico!'),]
+  _name='academy.materia'
+  _description='Materias del estudiante'
+  _columns={
+      'name':fields.char('Nombre')
+  }
+  _sql_constraints = [('name_uniq', 'unique (name)',
+      'El nombre de la Materia debe ser unico!'),]
+
+  print("##################################_sql_constraints", _sql_constraints)
--- models/prueba_order.py
+++ models/prueba_order.py
@@ -2,36 +2,36 @@
 from openerp import models, fields, api
 
 class res_partner(models.Model):
-    _name='res.partner'
-    _inherit='res.partner'
-    company_type = fields.Selection([('person', 'Individual'),
-                      ('company', 'Compañia'),
-                      ('is_school','Escuela')
-                      # ('student','Estudiante')
-                      ],
-           string='Company Type',
-           help='Technical field, used only to display a boolean using a radio '
-                'button. As for Odoo v9 RadioButton cannot be used on boolean '
-                'fields, this one serves as interface. Due to the old API '
-                'limitations with interface function field, we implement it '
-                'by hand instead of a true function field. When migrating to '
-                'the new API the code should be simplified.')
+  _name='res.partner'
+  _inherit='res.partner'
+  company_type = fields.Selection([('person', 'Individual'),
+                    ('company', 'Compañia'),
+                    ('is_school','Escuela')
+                    # ('student','Estudiante')
+                    ],
+         string='Company Type',
+         help='Technical field, used only to display a boolean using a radio '
+              'button. As for Odoo v9 RadioButton cannot be used on boolean '
+              'fields, this one serves as interface. Due to the old API '
+              'limitations with interface function field, we implement it '
+              'by hand instead of a true function field. When migrating to '
+              'the new API the code should be simplified.')
     # student_id = fields.Many2one('academy.student', 'Estudiante')        
 class academy_student(models.Model):
-    _name='academy.student'
-    _description='modelo de formulario para estudiante'
-    name=fields.Char('Nombre', size=128)
-    last_name=fields.Char('Apellido', size=128, required=True)
-    photo=fields.Binary('Fotografía')
-    create_date=fields.Datetime('Fecha Creación', readonly=True)
-    notes=fields.Html('Comentarios')
-    active=fields.Boolean('Activo')
-    state = fields.Selection([('draft','Documento Borrador'),
-                             ('progress','Progeso'),
-                             ('done','Egresado'),], 'Estado',default="draft")
-    age=fields.Integer('Edad', required=True)
-    partner_id=fields.Many2one('res.partner','Escuela')
-    calificaciones_id=fields.One2many('academy.calificacion','student_id','Calificaciones')
+  _name='academy.student'
+  _description='modelo de formulario para estudiante'
+  name=fields.Char('Nombre', size=128)
+  last_name=fields.Char('Apellido', size=128, required=True)
+  photo=fields.Binary('Fotografía')
+  create_date=fields.Datetime('Fecha Creación', readonly=True)
+  notes=fields.Html('Comentarios')
+  active=fields.Boolean('Activo')
+  state = fields.Selection([('draft','Documento Borrador'),
+                           ('progress','Progeso'),
+                           ('done','Egresado'),], 'Estado',default="draft")
+  # age=fields.Integer('Edad', required=True)
+  partner_id = fields.Many2one('res.partner','Escuela')
+  calificaciones_ids = fields.One2many('academy.calificacion','student_id','Calificaciones')
 
 
    #_order = 'name'
